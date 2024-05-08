3a6548e84af219f809a7518cc3e64463c71692f1
Zin Linn Htet <mackk5504@gmail.com>
2024-05-08 23:18:27 +0630

    Inprogress Dapper Service

---
+++ DotNet8.Dapper.RestApi.DbService/SqlConnectionStrings.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace DotNet8.Dapper.RestApi.DbService;
+
+public static class SqlConnectionStrings
+{
+    public static SqlConnectionStringBuilder connectionString = new SqlConnectionStringBuilder
+    {
+        DataSource = ".",
+        InitialCatalog = "TestDb",
+        UserID = "sa",
+        Password = "sasa@123",
+        TrustServerCertificate = true
+    };
+}
