601fca5458d787c7e49c7a246a8d3ddcbcb7ad09
Zin Linn Htet <mackk5504@gmail.com>
2024-05-08 23:54:25 +0630

    Complete Dapper Service

---
+++ DotNet8.Dapper.RestApi.DbService/Services/DapperServices.cs
@@ -0,0 +1,29 @@
+﻿namespace DotNet8.Dapper.RestApi.DbService;
+
+public class DapperServices : IDapperServices
+{
+    private readonly string _connection;
+
+    public DapperServices(string connection) => _connection = connection;
+
+    public List<T> Query<T>(string query, object? param = null)
+    {
+        IDbConnection dbConnection = new SqlConnection(_connection);
+        var lst = dbConnection.Query<T>(query, _connection).ToList();
+        return lst;
+    }
+
+    public T QueryFirstOrDefault<T>(string query, object? param = null)
+    {
+        IDbConnection dbConnection = new SqlConnection(_connection);
+        var item = dbConnection.Query<T>(query, _connection).FirstOrDefault();
+        return item;
+    }
+
+    public int Execute(string query, object? param = null)
+    {
+        IDbConnection dbConnection = new SqlConnection(_connection);
+        var result = dbConnection.Execute(query, param);
+        return result;
+    }
+}
