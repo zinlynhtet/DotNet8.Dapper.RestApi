368eed07912bd8c515ebb179b1947f48380b769c
Zin Linn Htet <mackk5504@gmail.com>
2024-05-08 23:17:57 +0630

    InProgress Dapper Service

---
+++ DotNet8.Dapper.RestApi.DbService/DapperServices.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace DotNet8.Dapper.RestApi.DbService;
+
+public class DapperServices
+{
+    private readonly string _connection;
+
+    public DapperServices(string connection) => _connection = connection;
+
+    public List<T> Query<T>(string query, object? param = null)
+    {
+        IDbConnection dbConnection = new SqlConnection(_connection);
+        var lst = dbConnection.Query<T>(query, _connection).toList();
+    }
+}
