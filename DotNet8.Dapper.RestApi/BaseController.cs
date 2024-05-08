c0e4befd95d4d99fcd38bf9cfc99f6d4ddd96cee
Zin Linn Htet <mackk5504@gmail.com>
2024-05-08 23:53:45 +0630

    Complete Base Controller

---
+++ DotNet8.Dapper.RestApi/BaseController.cs
@@ -0,0 +1,11 @@
+﻿namespace DotNet8.Dapper.RestApi;
+
+[ApiController]
+public class BaseController : ControllerBase
+{
+    [NonAction]
+    protected IActionResult InternalSeverError(Exception ex)
+    {
+        return Ok(ex);
+    }
+}
