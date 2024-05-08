63e5f7c5f780a9508c2b8fb5f053376fd48ff25b
Zin Linn Htet <mackk5504@gmail.com>
2024-05-08 23:55:46 +0630

    Complete Dapper Interface

---
+++ DotNet8.Dapper.RestApi.DbService/Services/IDapperServices.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNet8.Dapper.RestApi.DbService;
+
+public interface IDapperServices
+{
+    List<T> Query<T>(string query, object? param = null);
+    T QueryFirstOrDefault<T>(string query, object? param = null);
+    int Execute(string query, object? param = null);
+}
