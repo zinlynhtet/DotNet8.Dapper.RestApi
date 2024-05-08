026a2fb87cfd8adb812e1f76cef89fb6388e1fee
Zin Linn Htet <mackk5504@gmail.com>
2024-05-08 23:54:01 +0630

    Complete Blog Data Model

---
+++ DotNet8.Dapper.RestApi.DbService/Models/BlogDataModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNet8.Dapper.RestApi.DbService.Models;
+
+[Table("Tbl_Blog")]
+public class BlogDataModel
+{
+    [Key] public int BlogId { get; set; }
+    public string? BlogTitle { get; set; }
+    public string? BlogAuthor { get; set; }
+    public string? BlogContent { get; set; }
+}
