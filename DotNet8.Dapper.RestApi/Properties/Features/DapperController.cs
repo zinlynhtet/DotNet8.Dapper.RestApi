2a4ef09f056553a633fa5a0a3bb0376891cd426e
Zin Linn Htet <mackk5504@gmail.com>
2024-05-08 23:54:13 +0630

    Api Set Up

---
+++ DotNet8.Dapper.RestApi/Properties/Features/DapperController.cs
@@ -0,0 +1,36 @@
+﻿namespace DotNet8.Dapper.RestApi.Properties.Features;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DapperController : BaseController
+{
+    public async Task<IActionResult> Get()
+    {
+        return Ok();
+    }
+
+    public async Task<IActionResult> GetById(int id)
+    {
+        return Ok();
+    }
+
+    public async Task<IActionResult> Create(BlogDataModel reqModel)
+    {
+        return Ok();
+    }
+
+    public async Task<IActionResult> Put(int id, BlogDataModel reqModel)
+    {
+        return Ok();
+    }
+
+    public async Task<IActionResult> Patch(int id, BlogDataModel reqModel)
+    {
+        return Ok();
+    }
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        return Ok();
+    }
+}
