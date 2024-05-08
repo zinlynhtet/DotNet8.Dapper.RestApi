46c04f540aa1353434e565b7eb55ab84344c953b
Zin Linn Htet <mackk5504@gmail.com>
2024-05-08 23:55:28 +0630

    Extract Global Using

---
+++ DotNet8.Dapper.RestApi.DbService/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿// Global using directives
+
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using System.Data;
+global using Dapper;
+global using Microsoft.Data.SqlClient;
