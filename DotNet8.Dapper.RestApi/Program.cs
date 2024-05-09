29c19dd3783cf9be48cb8b48caef685aee6a4894
Zin Linn Htet <mackk5504@gmail.com>
2024-05-10 00:04:07 +0630

    Complete Register Service

--- DotNet8.Dapper.RestApi/Program.cs
+++ DotNet8.Dapper.RestApi/Program.cs
@@ -1,5 +1,4 @@
-using System.Net.Mime;
-using DotNet8.Dapper.RestApi.DbService;
+using DotNet8.Dapper.RestApi.DbService.Services;
 using DotNet8.Dapper.RestApi.Properties.Features;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<DapperServices>();
+
+builder.Services.AddScoped(n=> new DapperServices(SqlConnectionStrings.connectionString.ConnectionString));
 builder.Services.AddScoped<IApplication, Application>();
 var app = builder.Build();
 
