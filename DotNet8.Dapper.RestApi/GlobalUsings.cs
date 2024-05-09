0eebd1c31e8a1c2edbb78441561d405143d7d99b
Zin Linn Htet <mackk5504@gmail.com>
2024-05-10 00:03:31 +0630

    -

--- DotNet8.Dapper.RestApi/GlobalUsings.cs
+++ DotNet8.Dapper.RestApi/GlobalUsings.cs
@@ -1,6 +1,9 @@
 ﻿// Global using directives
 
+global using System.Data;
+global using Dapper;
 global using DotNet8.Dapper.RestApi.DbService;
 global using DotNet8.Dapper.RestApi.DbService.Models;
 global using DotNet8.Dapper.RestApi.Properties.Data;
 global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.Data.SqlClient;
