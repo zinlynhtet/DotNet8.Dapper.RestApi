93f1470552dda4bf4ac2625dc15b0fe01ae592ee
Zin Linn Htet <mackk5504@gmail.com>
2024-05-09 14:45:54 +0630

    Add Services

--- DotNet8.Dapper.RestApi/Program.cs
+++ DotNet8.Dapper.RestApi/Program.cs
@@ -1,3 +1,7 @@
+using System.Net.Mime;
+using DotNet8.Dapper.RestApi.DbService;
+using DotNet8.Dapper.RestApi.Properties.Features;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<DapperServices>();
+builder.Services.AddScoped<Application>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
