b8787c29c8d44db8e9c32b8d9d1bdaa081abdc09
Zin Linn Htet <mackk5504@gmail.com>
2024-05-09 15:12:04 +0630

    Complete Interface

---
+++ DotNet8.Dapper.RestApi/Properties/Features/IApplication.cs
@@ -0,0 +1,12 @@
+﻿namespace DotNet8.Dapper.RestApi.Properties.Features;
+
+public interface IApplication
+{
+    Task<List<BlogDataModel>> GetBlogs();
+    Task<BlogDataModel> GetBlogById(int id);
+    Task<int> CreateBlog(BlogDataModel reqModel);
+    Task<int> PutBlog(int id, BlogDataModel reqModel);
+    Task<int> PatchBlog(int id, BlogDataModel reqModel);
+    Task<int> DeleteBlog(int id);
+    Task<BlogDataModel>? FindById(int id);
+}
