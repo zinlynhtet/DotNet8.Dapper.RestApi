37b7c311192f2f529479e0499d4cb1f721b3981b
Zin Linn Htet <mackk5504@gmail.com>
2024-05-09 15:06:45 +0630

    Add Param Class

---
+++ DotNet8.Dapper.RestApi/Properties/Data/DapperParameters.cs
@@ -0,0 +1,17 @@
+﻿namespace DotNet8.Dapper.RestApi.Properties.Data;
+
+public class DapperParameters
+{
+    public DapperParameters()
+    {
+    }
+
+    public DapperParameters(string name, object value)
+    {
+        Name = name;
+        Value = value;
+    }
+
+    public string Name { get; set; }
+    public Object Value { get; set; }
+}
