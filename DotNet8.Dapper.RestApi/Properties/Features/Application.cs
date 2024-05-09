4d7ac3b3dbcced2ba47d5f6331d0094f4d11c047
Zin Linn Htet <mackk5504@gmail.com>
2024-05-09 14:45:32 +0630

    Complete Get and GetBy Id

---
+++ DotNet8.Dapper.RestApi/Properties/Features/Application.cs
@@ -0,0 +1,25 @@
+﻿namespace DotNet8.Dapper.RestApi.Properties.Features;
+
+public class Application
+{
+    private readonly DapperServices _dapperServices;
+
+    public Application(DapperServices dapperServices)
+    {
+        _dapperServices = dapperServices;
+    }
+
+    public async Task<List<BlogDataModel>> GetBlogs()
+    {
+        var query = Queries.GetAllQuery;
+        var lst = _dapperServices.Query<BlogDataModel>(query);
+        return lst;
+    }
+
+    public async Task<BlogDataModel> GetBlogById()
+    {
+        var query = Queries.GetByIdQuery;
+        var item = _dapperServices.QueryFirstOrDefault<BlogDataModel>(query);
+        return item;
+    }
+}
